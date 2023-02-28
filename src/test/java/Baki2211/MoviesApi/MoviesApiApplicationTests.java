295be4a590729c94ac0b586e1f2b8bbc163e2074
baki22 <angelopane22@gmail.com>
2023-02-28 17:48:19 +0000

    commit files

---
+++ src/test/java/Baki2211/MoviesApi/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Baki2211.MoviesApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
